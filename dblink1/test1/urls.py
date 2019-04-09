118a4a6c8fca9024b48a86e8055fbbf97cfdaf87
Justin F <fu.justin.s@gmail.com>
2019-04-09 14:25:18 -0700

    Add files via upload

---
+++ dblink1/test1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = 'test1'
+urlpatterns = [
+    path('', views.test1_home, name='test1_home'),
+    path('signup/', views.test1_signup, name='test1_signup'),
+]
