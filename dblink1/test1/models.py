7c21249a353bd8ab3f6d8bcc928f7ef59b0d6886
Justin F <fu.justin.s@gmail.com>
2019-04-09 10:35:43 -0700

    Uploaded User1 model

--- dblink1/test1/models.py
+++ dblink1/test1/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+from .managers import User1Manager
 # Create your models here.
+
+
+class User1(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email_address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    created_in = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = User1Manager()
+
+    def __str__(self):
+        return self.email
