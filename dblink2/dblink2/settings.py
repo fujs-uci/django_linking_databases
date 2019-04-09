f32fcb7201df98236a8e74fa7cd94141663054af
Justin F <fu.justin.s@gmail.com>
2019-04-09 11:32:00 -0700

    Linked dblink1 and dblink2 w/ same auth user model
    Imported sys to gain access to dblink1 auth user model. Then installed it into apps for dblink2. Can create super users from both projects cmd and login from both.

--- dblink2/dblink2/settings.py
+++ dblink2/dblink2/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
+sys.path.append('C:/Users/Justin/Desktop/Justin_Fu_Work/rebuild_bwanaz/testing/dblink1')
+import test1
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test2.apps.User1Config',
+    'test1.apps.User1Config',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Customer base user model
+
+AUTH_USER_MODEL = 'test1.User1'
