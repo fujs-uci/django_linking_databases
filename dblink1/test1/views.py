ee502f2a408559017a7d5d0971850b6b8d0d8266
Justin F <fu.justin.s@gmail.com>
2019-04-10 12:36:14 -0700

    Added Object2 search and result

--- dblink1/test1/views.py
+++ dblink1/test1/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from test2.models import *
+from .forms import *
 # Create your views here.
+
+
 def test1_home(request):
     user = request.user
     if user.is_authenticated and user.test_type() != 1:
@@ -8,7 +12,21 @@ def test1_home(request):
     if not user.is_authenticated:
         user = 'None'
 
-    context = {'user': user, }
+    search_form = SearchObject2()
+    search_request = request.GET.get('name')
+
+    if search_request:
+        if search_request is None or search_request.strip() == '':
+            return redirect('test1:test1_home')
+        context = {'query': Object2.objects.filter(name__contains=search_request).all(), }
+        template = 'test1/test1_result.html'
+        return render(request, template, context)
+    else:
+        if search_request is not None:
+            return redirect('test1:test1_home')
+
+    context = {'user': user,
+               'search_form': search_form, }
     template = 'test1/test1_home.html'
     return render(request, template, context)
 
@@ -20,9 +38,9 @@ def test1_signup(request):
             signup_form.save()
             email = signup_form.cleaned_data.get('email')
             raw_password = signup_form.cleaned_data.get('password1')
+            User1.objects.get(email=email).set_test_type(1)
             user = authenticate(email=email, password=raw_password)
             login(request, user)
-            User1.objects.get(email=email).set_test_type(1)
             return redirect('test1:test1_home')
 
     else:
