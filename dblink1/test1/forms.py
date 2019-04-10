3a89f86ce215dcf097f045224662bb084f9a8e47
Justin F <fu.justin.s@gmail.com>
2019-04-10 12:37:10 -0700

    Added Object2 query form

--- dblink1/test1/forms.py
+++ dblink1/test1/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from django.forms import ModelForm, TextInput, CharField
 from .models import User1
+from test2.models import *
 
 
 class User1CreationForm(UserCreationForm):
@@ -15,3 +16,14 @@ class User1ChangeForm(UserChangeForm):
     class Meta:
         model = User1
         fields = ('email', )
+
+
+class SearchObject2(ModelForm):
+
+    name = CharField(required=False)
+
+    class Meta:
+        model = Object2
+        fields = ('name',)
+
+        widgets = {'name': TextInput(attrs={'placeholder': 'Name'}, ), }
