01286977b0c19ae4da91fbdcc76876a83927e317
Justin F <fu.justin.s@gmail.com>
2019-04-10 10:55:21 -0700

    Update views.py

--- dblink2/test2/views.py
+++ dblink2/test2/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from .forms import *
+
 # Create your views here.
 
 
@@ -16,6 +18,7 @@ def test2_home(request):
     return render(request, template, context)
 
 
+@login_required
 def test2_object_manage(request, user_id):
     if request.method == "POST":
         object_create_form = Object2CreationForm(request.POST)
